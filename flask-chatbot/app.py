41b3b5f1cfbc1db62aa68346464bb0a07719f6ef
Erik Velasquez <erikvelasquez.25@gmail.com>
2021-05-31 16:41:17 +0200

    Some changes

--- flask-chatbot/app.py
+++ flask-chatbot/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
+status = 0
+
 app = Flask(__name__)
 
 english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
@@ -14,8 +16,27 @@ def home():
 
 @app.route("/get")
 def get_bot_response():
+    global status
     userText = request.args.get('msg')
-    return str(english_bot.get_response(userText))
+
+    if( userText == "add"):
+        status = 1
+        return str("Agrega la nueva noticia")
+    
+    if( userText == "news"):
+        status = 0
+        return str("Mostrando nueva noticia")
+
+    if( userText == "exit"):
+        status = 0
+        return str("Reiniciando status del bot")
+    
+    if( status == 1):
+        status = 0
+        return str("Noticia Enviada para clasificar y Extraer información")
+
+    if (status == 0):
+        return str(english_bot.get_response(userText))
 
 
 if __name__ == "__main__":
